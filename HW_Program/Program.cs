5102b330638bf33462247aaf64a14b204f86bd90
Egor Orlenok <messaf@mail.ru>
2022-11-20 07:32:50 +1000

    edit program

--- HW_Program/Program.cs
+++ HW_Program/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void PrintStringArray(string[] arr )
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write(arr[i]);
+        if(i != (arr.GetLength(0) - 1))
+          Console.Write(", ");  
+    }
+    Console.Write("]");
+}
+
+int CheckArraySymbolsNum(string[] arrIn, int symMum)
+{
+    int counter = 0;
+
+    for (int i = 0; i < arrIn.GetLength(0); i++)
+        if(arrIn[i].Length <= symMum)
+            counter++;  
+
+    return counter;        
+}
+
+string[] ArrayProcess(string[] arrIn, int symMum)
+{
+    int indexOut = 0;
+    string[] arrOut = new string[CheckArraySymbolsNum(arrIn, symMum)]; 
+
+    for (int i = 0; i < arrIn.GetLength(0); i++)
+    {
+        if(arrIn[i].Length <= symMum)
+        {
+            arrOut[indexOut] = arrIn[i];
+            indexOut++;
+        }
+    }
+
+    return arrOut;
+}
+
+Console.Write("\r\n=========== Контрольная работа =============\r\n");
+
+string[] inArray = {"reg", "27", "Egor", "Orlenok", "3", "5", "years"};
+Console.Write("Исходный массив: "); PrintStringArray(inArray); Console.WriteLine();
+
+string[] outArray = ArrayProcess(inArray, 3);
+Console.Write("Полученный массив: "); PrintStringArray(outArray); Console.WriteLine();
